90234529a41ada64a59a124948e45417efa3f17d
vulongepu <vulongepu@gmail.com>
2016-04-05 10:18:03 +0700

    fix padding

---
+++ css/xoa.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>vong lap for</title>
+</head>
+<body>
+	<table class="table">
+		<tr>
+			<th></th>
+			<th>Số bắt đầu</th>
+			<th><input type="text" name="" id=""></th>
+			<th>Số kết thúc</th>
+			<th><input type="text" name="" id=""></th>
+		</tr>
+		<tr>Kết quả</tr>
+		<tr>
+			<th>Tổng các số</th>
+			<th></th>
+		</tr>
+		<tr>
+			<th>Tích các số</th>
+			<th></th>
+		</tr>
+		<tr>
+			<th>Tổng các số chẵn</th>
+			<th></th>
+		</tr>
+		<tr>
+			<th>Tổng các số lẻ</th>
+			<th></th>
+		</tr>
+		<tr><input type="button" value="Tính toán"></tr>
+	</table>
+</body>
+</html>
